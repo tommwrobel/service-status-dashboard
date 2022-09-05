cb0049a8da7d671c6a0ff2956a10097c4bdba68c
Tomasz Wrobel <t.wrobel@sap.com>
2022-09-05 15:45:50 +0200

    refactor: moved configs to files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,31 +4,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { createTheme, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import ApplicationBar from "./components/ApplicationBar/ApplicationBar";
 import CssBaseline from '@mui/material/CssBaseline';
 import EnvironmentPage from "./components/EnvironmentPage/EnvironmentPage";
 import { ReactQueryDevtools } from "react-query/devtools";
-
-// TODO: move to separate config file
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: 2,
-            refetchOnWindowFocus: false,
-            suspense: false,
-            staleTime: 1000 * 60 * 10,
-            refetchInterval: false,
-        },
-    },
-});
-
-// TODO: move to separate config file
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-});
+import { darkTheme } from "./config/ThemeConfig";
+import { queryClient } from "./config/ReactQueryConfig";
 
 function App() {
 

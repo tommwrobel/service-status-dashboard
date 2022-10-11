3592c2d5f51a6913fc183d2ebbc3addf8fe2e137
Kamil Wrobel <kamil.wrobel@symmetrical.ai>
2022-10-11 08:57:52 +0200

    Add logs + refactor configs

--- server/utls/envsConfigParser.js
+++ server/utls/envsConfigParser.js
@@ -3,21 +3,22 @@ module.exports.parseEnvsConfig = function (config) {
         envs: []
     }
 
-    const environments = Object.keys(config.envs)
-
-    environments.forEach(env => {
+    config.envs.forEach(env => {
         const envConfig = {
             name: env,
             configUrl: config.envConfigUrl + env,
-            services: Object.keys(config.envs[env]).map(serviceName => ({
+            services: Object.keys(config.services)
+            .sort()
+            .map(serviceName => ({
                 name: serviceName,
                 branch: config.services[serviceName]?.branch,
-                appUrl: config.envs[env][serviceName],
-                appHealthUrl: config.envs[env][serviceName] + config.servicePaths.health,
-                appInfoUrl: config.envs[env][serviceName] + config.servicePaths.info,
+                appUrl: config.services[serviceName].url.replace('{{ENV}}', env),
+                appHealthUrl: config.services[serviceName].url.replace('{{ENV}}', env) + config.servicePaths.health,
+                appInfoUrl: config.services[serviceName].url.replace('{{ENV}}', env) + config.servicePaths.info,
                 repositoryUrl: config.services[serviceName].repository + '/commits/branch/' + config.services[serviceName]?.branch,
                 jenkinsUrl: config.services[serviceName].jenkins,
-                swaggerUrl: config.envs[env][serviceName] + config.servicePaths.swagger
+                swaggerUrl: config.services[serviceName].url.replace('{{ENV}}', env) + config.servicePaths.swagger,
+                logs: encodeURI(config.envAppLogsUrl.replace('{{APP}}', serviceName).replace('{{ENV}}', env))
             }))
         }
         output.envs.push(envConfig)
